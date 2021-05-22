3ee82902d09fd96e1241df1fbb076c71d151ff6a
FelipeSilva99 <felipe.rl.silva@hotmail.com>
2021-05-22 19:35:23 -0300

    First comment

---
+++ index.js
@@ -0,0 +1,13 @@
+const nome ='Felipe Silva';
+
+let idade = 22;
+let ano = 2021;
+
+const human = true
+
+const number1 = 3
+const number2 = 6
+
+console.log('Meu ano de nascimento é:', ano - idade)
+console.log('Você é um humano?', human)
+console.log('A soma entre os dois valores é:', number1 + number2)
